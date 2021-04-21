bcd89f29c4632bed7b3bddda5b9704623c301db5
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-21 22:24:45 +0300

    contact deletion test failed contact send email test passed

--- addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
@@ -2,7 +2,7 @@ package addressbook.tests;
 
 import java.util.concurrent.TimeUnit;
 
-import addressbook.ContactData;
+import addressbook.model.ContactData;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTests {
   private WebDriver wd;
-//  private String baseUrl;
+  //  private String baseUrl;
 //  private boolean acceptNextAlert = true;
 //  private StringBuffer verificationErrors = new StringBuffer();
 
@@ -32,15 +32,10 @@ public class ContactCreationTests {
     wd.findElement(By.id("LoginForm")).submit();
   }
 
+
   @Test
   public void testContactCreation() throws Exception {
-//    wd.get("http://localhost/addressbook/index.php");
-//    wd.findElement(By.name("user")).clear();
-//    wd.findElement(By.name("user")).sendKeys("admin");
-//    wd.findElement(By.name("pass")).click();
-//    wd.findElement(By.name("pass")).clear();
-//    wd.findElement(By.name("pass")).sendKeys("secret");
-//    wd.findElement(By.id("LoginForm")).submit();
+
     gotoAddNewPage();
     fillContactForm(new ContactData("aleksandr", "tysevich", "tver, ippodromnaya 1", "+79106465892", "al-tysevi4@yandex.ru", "11", "20", "October", "1970"));
     submitContactCreation();
@@ -59,29 +54,29 @@ public class ContactCreationTests {
   private void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("firstname")).sendKeys("aleksandr");
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("lastname")).sendKeys("tysevich");
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("address")).sendKeys("tver, ippodromnaya 1");
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("mobile")).sendKeys("+79106465892");
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("email")).sendKeys("al-tysevi4@yandex.ru");
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBdayear());
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("11");
     wd.findElement(By.xpath("//option[@value='11']")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("20");
     wd.findElement(By.xpath("//option[@value='20']")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("October");
     wd.findElement(By.xpath("//option[@value='October']")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
+    wd.findElement(By.name("byear")).sendKeys("1970");
   }
 
   private void gotoAddNewPage() {
@@ -97,25 +92,7 @@ public class ContactCreationTests {
 //    }
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-//  private String closeAlertAndGetItsText() {
+  //  private String closeAlertAndGetItsText() {
 //    try {
 //      Alert alert = driver.switchTo().alert();
 //      String alertText = alert.getText();
--- addressbook-web-test/src/test/java/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/addressbook/tests/TestBase.java
@@ -1,21 +1,23 @@
 package addressbook.tests;
 
 import addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
     protected final ApplicationManager app = new ApplicationManager();
+    protected WebDriver wd;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
     }
 
+
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
-
 }
