2256f44878c48dbdbb4485df76c34518da3e2ee3
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-22 22:11:12 +0300

    ContactSendEmailTests

--- addressbook-web-test/src/test/java/addressbook/tests/ContactSendEmailTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/ContactSendEmailTests.java
@@ -6,7 +6,6 @@ import org.testng.annotations.*;
 
 public class ContactSendEmailTests extends TestBase{
 
-
   @Test
   public void testContactSendEmail() throws Exception {
     app.getContactHelper().checkTheContact();
