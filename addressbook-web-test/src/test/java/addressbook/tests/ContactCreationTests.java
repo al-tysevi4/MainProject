bd6a78f24c521bc6e34961629894f481deaf4059
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-22 11:40:20 +0300

    functions ContactDeletionAllTests made

--- addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
@@ -9,29 +9,10 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ContactCreationTests {
-  private WebDriver wd;
+public class ContactCreationTests extends TestBase{
   //  private String baseUrl;
 //  private boolean acceptNextAlert = true;
 //  private StringBuffer verificationErrors = new StringBuffer();
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    //baseUrl = "https://www.google.com/";
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/index.php");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.id("LoginForm")).submit();
-  }
 
 
   @Test
@@ -41,56 +22,6 @@ public class ContactCreationTests {
     fillContactForm(new ContactData1("aleksandr", "tysevich", "tver, ippodromnaya 1", "+79106465892", "al-tysevi4@yandex.ru", "20", "October", "1970"), "aleksandr", "tysevich", "tver, ippodromnaya 1", "+79106465892", "al-tysevi4@yandex.ru", "20", "October", "1970");
     submitContactCreation();
     homeAndLogout();
-  }
-
-  private void homeAndLogout() {
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  private void submitContactCreation() {
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-  }
-
-  private void fillContactForm(ContactData1 contactData1, String aleksandr, String tysevich, String s, String s1, String s2, String s3, String october, String s4) {// String aleksandr, String tysevich, String s, String s1, String s2, String s3, String october, String s4) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData1.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData1.getLastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData1.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData1.getMobile());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData1.getEmail());
-    wd.findElement(By.name("bday")).click();
-//    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("11");
-//    wd.findElement(By.xpath("//option[@value='11']")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData1.getBday());
-    wd.findElement(By.xpath("//option[@value='20']")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData1.getBmonth());
-    wd.findElement(By.xpath("//option[@value='October']")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData1.getByear());
-  }
-
-  private void gotoAddNewPage() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-//    String verificationErrorString = verificationErrors.toString();
-//    if (!"".equals(verificationErrorString)) {
-//      fail(verificationErrorString);
-//    }
   }
 
   //  private String closeAlertAndGetItsText() {
--- addressbook-web-test/src/test/java/addressbook/tests/ContactDeletionAllTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/ContactDeletionAllTests.java
@@ -1,54 +1,63 @@
 package addressbook.tests;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
-public class ContactDeletionAllTests {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+public class ContactDeletionAllTests extends TestBase {
+  //private WebDriver wd;
+//  private String baseUrl;
+//  private boolean acceptNextAlert = true;
+//  private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+//  @BeforeMethod(alwaysRun = true)
+//  public void setUp() throws Exception {
+//    wd = new FirefoxDriver();
+//    //baseUrl = "https://www.google.com/";
+//    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//  }
 
   @Test
-  public void testContactDeletionAllTests() throws Exception {
-    driver.get("http://localhost/addressbook/index.php");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.id("LoginForm")).submit();
-    driver.findElement(By.id("MassCB")).click();
-    acceptNextAlert = true;
-    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+  public void testContactDeletion() throws Exception {
+//    wd.get("http://localhost/addressbook/index.php");
+//    wd.findElement(By.name("user")).clear();
+//    wd.findElement(By.name("user")).sendKeys("admin");
+//    wd.findElement(By.name("pass")).click();
+//    wd.findElement(By.name("pass")).clear();
+//    wd.findElement(By.name("pass")).sendKeys("secret");
+//    wd.findElement(By.id("LoginForm")).submit();
+    selectAllContacts();
+    //acceptNextAlert = true;
+    deleteSelectedGroups();
     //assertTrue(closeAlertAndGetItsText().matches("^Delete 8 addresses[\\s\\S]$"));
-    driver.switchTo().alert().accept();
+    extracted();
   }
 
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
+  private void extracted() {
+    wd.switchTo().alert().accept();
   }
+
+  private void deleteSelectedGroups() {
+    wd.findElement(By.xpath("//input[@value='Delete']")).click();
+  }
+
+  private void selectAllContacts() {
+    wd.findElement(By.id("MassCB")).click();
+  }
+
+//  @AfterMethod(alwaysRun = true)
+//  public void tearDown() throws Exception {
+//    wd.quit();
+//    String verificationErrorString = verificationErrors.toString();
+//    if (!"".equals(verificationErrorString)) {
+//      fail(verificationErrorString);
+//    }
+//  }
 
   private boolean isElementPresent(By by) {
     try {
-      driver.findElement(by);
+      wd.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -57,25 +66,25 @@ public class ContactDeletionAllTests {
 
   private boolean isAlertPresent() {
     try {
-      driver.switchTo().alert();
+      wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
     }
   }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+//  private String closeAlertAndGetItsText() {
+//    try {
+//      Alert alert = driver.switchTo().alert();
+//      String alertText = alert.getText();
+//      if (acceptNextAlert) {
+//        alert.accept();
+//      } else {
+//        alert.dismiss();
+//      }
+//      return alertText;
+//    } finally {
+//      acceptNextAlert = true;
+//    }
+//  }
 }
