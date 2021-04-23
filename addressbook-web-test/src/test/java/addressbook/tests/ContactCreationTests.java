819f7c8cec031faf2686548ddcdc32caf6598e4d
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-23 22:56:30 +0300

    test before refactoring +++

--- addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/ContactCreationTests.java
@@ -1,18 +1,59 @@
 package addressbook.tests;
 
-import addressbook.model.ContactData1;
+
+import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class ContactCreationTests extends TestBase{
+public class ContactCreationTests {
+  private WebDriver wd;
 
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.id("LoginForm")).submit();
+  }
 
   @Test
   public void testContactCreation() throws Exception {
+    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("alex");
+    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.linkText("home page")).click();
+  }
 
-    app.getContactHelper().gotoAddNewPage();
-    app.getContactHelper().fillContactForm(new ContactData1("aleksandr", "tysevich", "tver, ippodromnaya 1", "+79106465892", "al-tysevi4@yandex.ru", "20", "October", "1970"), "aleksandr", "tysevich", "tver, ippodromnaya 1", "+79106465892", "al-tysevi4@yandex.ru", "20", "October", "1970");
-    app.getContactHelper().submitContactCreation();
-    app.getContactHelper().homeAndLogout();
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
   }
 }
--- addressbook-web-test/src/test/java/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/GroupDeletionTests.java
@@ -1,17 +1,57 @@
 package addressbook.tests;
 
+
+import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupDeletionTests extends TestBase{
+
+public class GroupDeletionTests {
+  private WebDriver wd;
 
 
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.id("LoginForm")).submit();
+  }
 
   @Test
   public void testGroupDeletion() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
+    wd.findElement(By.name("selected[]")).click();
+    wd.findElement(By.xpath("(//input[@name='delete'])[2]")).click();
+    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.linkText("Logout")).click();
   }
 
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 }
