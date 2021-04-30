b0ed4e14416df347a8d6df27c1b5e2dc986e81c6
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-30 20:50:37 +0300

    GroupCreationTests с лямбда выражением проходит

--- addressbook-web-test/src/test/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,20 +18,15 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     //int before = app.getGroupHelper().getGroupCount();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size() + 1 );
 
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
