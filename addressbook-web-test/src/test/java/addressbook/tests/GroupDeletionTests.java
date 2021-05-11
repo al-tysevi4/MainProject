0a10c19bf4637d7d3b9b955d40d6daadcad8b798
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-05-11 15:28:03 +0300

    Fluent- интерфейсы в тестах для групп

--- addressbook-web-test/src/test/java/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/addressbook/tests/GroupDeletionTests.java
@@ -10,22 +10,33 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
-
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().groupPage();
+    if (app.group().list().size() == 0) {
+      app.group().create(new GroupData().withName("test1"));
+    }
+  }
   @Test
   public void testGroupDeletion() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    }
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().checkGroup(before.size() - 1);
-    app.getGroupHelper().deleteGroup();
-    app.getGroupHelper().returnToGroupPage();
-    List<GroupData> after = app.getGroupHelper().getGroupList();
+//    app.getNavigationHelper().gotoGroupPage();
+//    if (! app.getGroupHelper().isThereAGroup()){
+//      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+//    }
+    List<GroupData> before = app.group().list();
+    int index = before.size() - 1;
+    delete(index);
+    List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() -1);
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     Assert.assertEquals(before, after);
 
+  }
+
+  private void delete(int index) {
+    app.group().checkGroup(index);
+    app.group().deleteGroup();
+    app.group().returnToGroupPage();
   }
 }
