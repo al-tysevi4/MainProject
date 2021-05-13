fcea6105da1c249309f5b4836916045326dc4590
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-05-13 18:15:48 +0300

    реализовано кеширование и хеширование в тестах для групп

--- addressbook-web-test/src/test/java/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-test/src/test/java/addressbook/appmanager/GroupHelper.java
@@ -53,6 +53,7 @@ public class GroupHelper extends HelperBase {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache = null;
         returnToGroupPage();
     }
     public void modify(GroupData group) {
@@ -60,31 +61,38 @@ public class GroupHelper extends HelperBase {
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
+        groupCache = null;
         returnToGroupPage();
     }
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
     }
 
-    public int getGroupCount() {
+    public int count() {
        return wd.findElements(By.name("selected[]")).size();
     }
 
+    private Groups groupCache = null;
+
     public Groups all() {
-        Groups groups = new Groups();
+        if (groupCache != null) {
+            return new Groups(groupCache);
+        }
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             //GroupData group = new GroupData().withId(id).withName(name);
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
     }
 
     public void delete(GroupData group) {
         checkGroupById(group.getId());
         deleteGroup();
+        groupCache = null;
         returnToGroupPage();
     }
 }
