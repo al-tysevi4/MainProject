cd25a02123e678c406ce6f48e7ca4c9b9f797489
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-05-21 15:52:12 +0300

    падает HbConnectionTest после добавления зависимости "logback-classic"

--- addressbook-web-test/src/test/java/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/addressbook/tests/TestBase.java
@@ -2,12 +2,19 @@ package addressbook.tests;
 
 import addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
@@ -17,5 +24,15 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+    }
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+
     }
 }
