66e1344d666731c973132c2c93d7fc1ecfcff26c
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-06-07 11:36:02 +0300

    не исчезает капча в RegistrationTests

--- mantis-tests/src/test/java/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/mantis/tests/RegistrationTests.java
@@ -1,7 +1,7 @@
 package mantis.tests;
 
 import mantis.model.MailMessage;
-import org.openqa.selenium.By;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
